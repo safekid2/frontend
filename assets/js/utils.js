93486356de829bbe186de0bcfde8f3beca13930e
SafeKids Admin <allioluwatosin8@gmail.com>
2025-06-01 01:34:52 +0100

    render implemented

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:5000/api/v1';
+const API_BASE_URL = 'https://safe-kids-qixg.onrender.com/api/v1';
 
 // Toast initialization
 const toastEl = document.getElementById('toast');
